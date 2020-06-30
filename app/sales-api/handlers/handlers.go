ba5686f9ddce68cbf831874e1d5bc08eae3047ca
Andrei Brabete <andreibrabete@gmail.com>
2020-06-30 14:53:25 +0100

    Updated the handlers for sales-api modle

--- app/sales-api/handlers/handlers.go
+++ app/sales-api/handlers/handlers.go
@@ -12,8 +12,6 @@ import (
 
 // API constructs an http.Handler with all application routes defined.
 func API(build string, shutdown chan os.Signal, log *log.Logger) http.Handler {
-
-	httptreemux.New()
-
-	return nil
+	tm := httptreemux.NewContextMux()
+	return tm
 }
